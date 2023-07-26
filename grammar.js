1c854081c5a511fa1c276bca5b8e7eaab863f4a5
Amaan Qureshi <amaanq12@gmail.com>
2023-07-26 09:02:19 -0400

    fix: allow annotations after new but before the type, add cent unicode to identifiers

--- grammar.js
+++ grammar.js
@@ -373,6 +373,7 @@ module.exports = grammar({
 
     _unqualified_object_creation_expression: $ => prec.right(seq(
       'new',
+      repeat($._annotation),
       field('type_arguments', optional($.type_arguments)),
       repeat($._annotation),
       field('type', $._simple_type),
@@ -1231,7 +1232,7 @@ module.exports = grammar({
     super: $ => 'super',
 
     // https://docs.oracle.com/javase/specs/jls/se8/html/jls-3.html#jls-IdentifierChars
-    identifier: $ => /[\p{L}_$][\p{L}\p{Nd}_$]*/,
+    identifier: $ => /[\p{L}_$][\p{L}\p{Nd}\u00A2_$]*/,
 
     // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
     comment: $ => choice(
